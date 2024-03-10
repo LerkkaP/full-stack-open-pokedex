5066c9194c44b2b50d205bc2182507308920dffb
Erik Peteri <peteri.erik@gmail.com>
2024-03-10 12:09:08 +0200

    exercise 11.9

---
+++ cypress/e2e/pokedex.cy.js
@@ -0,0 +1,16 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('Pokemon can be navigated to', function() {
+    cy.visit('http://localhost:5000');
+
+    cy.get('.list-item[href="/pokemon/squirtle"]').click();
+
+    cy.url().should('include', '/pokemon/squirtle');
+    cy.contains('squirtle')
+    cy.contains('rain dish')
+  })
+})
